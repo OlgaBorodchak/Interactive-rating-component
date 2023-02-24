f0e888436fc66c908c9cc6a2cc56894a19218a1e
OlgaBorodchak <olga.leonovna@gmail.com>
2023-02-24 10:38:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
+const form = document.querySelector(".rating-container");
 const btns = document.querySelectorAll(".rating-btn");
-const submitBtn = document.querySelector(".submit-btn");
 const getScore = document.querySelector(".score");
 const submitCard = document.querySelector(".submit-card");
 const thankYouCard = document.querySelector(".thank-you-card");
 
-
-btns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        removeFocus();
-    getScore.innerHTML = btn.innerHTML;
-    btn.classList.add("active");
-    })
-
-    removeFocus = () => {
-        btns.forEach(btn => {
-        btn.classList.remove("active"); 
-        })
-    }    
-})
-
-submitBtn.addEventListener("click", () => {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const rating = form.rating.value;
+    removeFocus();
+    getScore.textContent = rating;
     submitCard.classList.add("d-none");    
     thankYouCard.classList.remove("d-none");
 })
 
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        removeFocus();
+        btn.classList.add("active");
+        })
 
-
-
-
-
-
-
-
-
-
-
+    removeFocus = () => {
+    btns.forEach(btn => {
+        btn.classList.remove("active"); 
+        })
+    }    
+})
